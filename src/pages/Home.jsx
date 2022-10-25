44c38c85676efd0dae3b915c69a438be60d831c3
Saurav <sauravsing2205@gmail.com>
2022-10-25 19:06:41 +0530

    initial commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import image1 from "../img/homeI.png";
-import home1 from "../img/Home(1).svg"
-import home2 from "../img/Home2.svg"
-import home3 from "../img/home33.jpg"
+import home1 from "../img/Home(1).svg";
+import home2 from "../img/Home2.svg";
+import home3 from "../img/home33.jpg";
 import Blog from "./Blog";
 import CardWithMan from "./CardWithMan";
 import Footerr from "./Footerr";
@@ -11,38 +11,30 @@ import { LoginPage } from "./LoginPage";
 import Scroll from "./Scroll";
 import WeAre from "./WeAre";
 
-
-
-
 const Home = () => {
-  
   return (
     <>
-  
+      <div className="pt-24">
+        <img className="hideMe z-50 absolute" src={home1} />
+        <img className="hideMe2 absolute" src={home2} />
+      </div>
 
+      {/* <img className="hideMe3" src={home3} /> */}
 
-  
-
-  <img className="hideMe " src={home1} />
-  <img className="hideMe2 " src={home2} />
-  {/* <img className="hideMe3" src={home3} /> */}
-    
-   <section className="sm:w-screen backgroundImghead">
-
-      <div className="sm:flex justify-center  pt-24">   <div>
-</div>
-          <img src={image1}  />
+      <section className="sm:w-screen backgroundImghead">
+        <div className="sm:flex justify-center  ">
+          {" "}
+          <div></div>
+          <img src={image1} />
         </div>
-   </section>
-   <ImpTech/>
-   <WeAre/>
-   <CardWithMan/>
-   <Blog/>
-   <LoginPage/>
-   <Footerr/>
-      </>
-        
-  
+      </section>
+      <ImpTech />
+      <WeAre />
+      <CardWithMan />
+      <Blog />
+      <LoginPage />
+      <Footerr />
+    </>
   );
 };
 
